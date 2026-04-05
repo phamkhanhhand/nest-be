002df84c8c4c5c029b26202462032c3a94943a0c
phamkhanhhand <phamkhanhhand@gmail.com>
2026-04-05 19:38:30 +0700

    edit config

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,11 +29,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
           defineConfig({
             type: 'postgresql',
             // driver: PostgreSqlDriver,
-            host: config.get('DB_HOST', 'localhost'),
-            port: Number(config.get('DB_PORT', 5431)),
-            user: config.get('DB_USER', 'pkha'),
-            password: config.get('DB_PASS', 'pkha'),
-            dbName: config.get('DB_NAME', 'flexvalue'),
+            host: config.get('DB_HOST',  process.env.DB_HOST || 'localhost'),
+            port: Number(config.get('DB_PORT', process.env.DB_PORT || 5431)),
+            user: config.get('DB_USER', process.env.DB_USER || 'postgres'),
+            password: config.get('DB_PASS', process.env.DB_PASS || 'postgres'),
+            dbName: config.get('DB_NAME', process.env.DB_NAME || 'postgres'),
             // entities: ['./dist/entities'], // Adjust the path as needed
             // autoLoadEntities: true,
 
